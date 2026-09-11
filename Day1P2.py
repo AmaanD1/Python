2ec091d597f80e2c3d17b7f9d9d11a61a28de0cd
Amaan <adhiwows@gmail.com>
2026-09-11 09:56:21 +0530

    Update Day1P2.py

--- Day1P2.py
+++ Day1P2.py
@@ -1,4 +1,4 @@
-age_text=input("Enter your birth year:")
+age_text=input("Enter your birth year:") #Here you can Combine also in one line by input() by keeping the line inside it for one line, as its easy.
 age=int(age_text)
 
 current_age=2026-age
